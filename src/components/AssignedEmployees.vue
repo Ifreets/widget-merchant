9a1a423b19c3bb5b69d612b6ccf85ed15ba723b0
bbhhainx <hainx.bbh@gmail.com>
2024-06-01 14:26:52 +0700

    fix bug

--- src/components/AssignedEmployees.vue
+++ src/components/AssignedEmployees.vue
@@ -4,8 +4,8 @@
     class="max-h-[100px] overflow-y-auto scrollbar-thin"
   >
     <li
-      v-if="list_employee?.length"
-      v-for="item in list_employee"
+      v-if="props.list_employee?.length"
+      v-for="item in props.list_employee"
       class="flex justify-between items-center py-1 border-b"
     >
       <div class="flex items-center gap-2">
@@ -39,53 +39,13 @@
 import { useAppStore, useCommonStore } from '@/stores'
 //* import icon && image
 import AvatarDefault from '@/assets/imgs/default-avatar.png'
-import WIDGET from 'bbh-chatbox-widget-js-sdk'
-import { onMounted, ref } from 'vue'
-import { getAllEmployee } from '@/service/helper/getAllEmploy'
 
 //* props
-const list_employee = defineModel<string[]>('list_employee')
+const props = defineProps<{
+  /** danh sách nhân viên phụ trách */
+  list_employee: string[]
+}>()
 /** store */
 const commonStore = useCommonStore()
 const appStore = useAppStore()
-
-// lắng nghe sự kiện từ chatbox
-WIDGET.onEvent(async () => {
-  load()
-})
-
-onMounted(async () => {
-  load()
-})
-async function load() {
-  if (!list_employee.value) return
-  //lấy danh sách tất cả nhân viên
-  await fetchAllEmployee(
-    commonStore.id_business,
-    commonStore.token_business,
-    list_employee.value
-  )
-}
-/** hàm xử lý lấy tất cả nhân viên */
-async function fetchAllEmployee(
-  /** id doanh nghiệp */
-  id_business: string,
-  /** token doanh nghiệp */
-  token_business: string,
-  /** danh sách nhân viên được giao phụ trách khách hàng */
-  list_employee: string[]
-) {
-  try {
-    /** lấy danh sách toàn bộ nhân viên */
-    let result = await getAllEmployee(
-      id_business,
-      token_business,
-      list_employee
-    )
-    if (!result) return
-    commonStore.listAllEmployee = result
-  } catch (error) {
-    console.log('get all employee home', error)
-  }
-}
 </script>
