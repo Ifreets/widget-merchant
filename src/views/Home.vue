e37f2b658f94f3a60518c43ac1285a2d3c3351fb
bbhhainx <hainx.bbh@gmail.com>
2024-06-22 14:46:29 +0700

    improve ui - select-none

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,94 +1,101 @@
 <template>
-  <div class="w-full h-full flex justify-center items-center bg-slate-300">
-    <div class="w-screen h-screen sm:w-[395px] sm:h-[300px] bg-white text-sm">
-      <div
-        v-if="['USER', 'ORDER'].includes(appStore.tab)"
-        class="p-3 flex flex-col gap-2.5 text-gray-900"
-      >
-        <div class="w-full flex gap-2.5">
-          <Avatar
-            class="h-16 min-w-16 w-16 font-semibold text-3xl overflow-hidden"
-            :avatar="appStore?.getUserAvatar()"
-            :name="appStore?.getClientName() || ''"
-          />
-          <div class="w-full flex flex-col gap-1">
-            <div
-              class="h-fit w-full flex justify-between items-center border-b py-1"
-            >
-              <p class="text-base truncate w-52 xs:w-64">
-                <span class="font-medium">
-                  {{ appStore?.getClientName() }}
-                </span>
-                <span class="text-gray-900 ml-0.5 text-sm">{{
-                  appStore?.getClientID()
-                }}</span>
-              </p>
-              <a
-                :href="appStore?.getLinkToMerchant()"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img
-                  :src="InfoIcon"
-                  class="h-4 w-4"
-                />
-              </a>
-            </div>
-            <div class="font-medium">
-              <p class="text-sky-500">số điện thoại</p>
-              <p class="truncate w-60 text-gray-900">
-                {{ appStore?.customer_info?.phone }}
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="py-1 px-3 bg-slate-100 rounded-lg">
-          <div
-            class="flex py-2 text-center font-medium text-[#71717A] border-b"
-          >
-            <p
-              class="w-1/2 rounded py-1 cursor-pointer"
-              :class="
-                appStore.tab === 'USER' ? 'text-black bg-white shadow' : ''
-              "
-              @click="appStore.tab = 'USER'"
-            >
-              Nhân sự phụ trách
-            </p>
-            <p
-              class="w-1/2 rounded py-1 cursor-pointer"
-              :class="
-                appStore.tab === 'ORDER' ? 'text-black bg-white shadow' : ''
-              "
-              @click="appStore.tab = 'ORDER'"
-            >
-              Đơn hàng
-            </p>
-          </div>
-          <AssignedEmployees
-            v-if="appStore.tab === 'USER'"
-            :list_employee="list_employee"
-          />
-          <div v-if="appStore.tab === 'ORDER'">
-            <p class="text-center py-2">Tính năng đang được phát triển...</p>
-          </div>
-        </div>
+    <div class="w-full h-full flex justify-center items-center bg-slate-300">
         <div
-          v-if="WIDGET.is_admin"
-          class="flex justify-end"
+            class="w-screen h-screen sm:w-[395px] sm:h-[300px] bg-white text-sm"
         >
-          <p
-            class="bg-slate-200 text-slate-700 py-1 px-2 rounded-md cursor-pointer text-xs font-medium"
-            @click="appStore.tab = 'SETTING'"
-          >
-            Thiết lập
-          </p>
+            <div
+                v-if="['USER', 'ORDER'].includes(appStore.tab)"
+                class="p-3 flex flex-col gap-2.5 text-gray-900"
+            >
+                <div class="w-full flex gap-2.5">
+                    <Avatar
+                        class="h-16 min-w-16 w-16 font-semibold text-3xl overflow-hidden"
+                        :avatar="appStore?.getUserAvatar()"
+                        :name="appStore?.getClientName() || ''"
+                    />
+                    <div class="w-full flex flex-col gap-1">
+                        <div
+                            class="h-fit w-full flex justify-between items-center border-b py-1"
+                        >
+                            <p class="text-base truncate w-52 xs:w-64">
+                                <span class="font-medium">
+                                    {{ appStore?.getClientName() }}
+                                </span>
+                                <span class="text-gray-900 ml-0.5 text-sm">{{
+                                    appStore?.getClientID()
+                                }}</span>
+                            </p>
+                            <a
+                                :href="appStore?.getLinkToMerchant()"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <img
+                                    :src="InfoIcon"
+                                    class="h-4 w-4 select-none"
+                                />
+                            </a>
+                        </div>
+                        <div class="font-medium">
+                            <p class="text-sky-500">số điện thoại</p>
+                            <p class="truncate w-60 text-gray-900">
+                                {{ appStore?.customer_info?.phone }}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="py-1 px-3 bg-slate-100 rounded-lg">
+                    <div
+                        class="flex py-2 text-center font-medium text-[#71717A] border-b"
+                    >
+                        <p
+                            class="w-1/2 rounded py-1 cursor-pointer select-none"
+                            :class="
+                                appStore.tab === 'USER'
+                                    ? 'text-black bg-white shadow'
+                                    : ''
+                            "
+                            @click="appStore.tab = 'USER'"
+                        >
+                            Nhân sự phụ trách
+                        </p>
+                        <p
+                            class="w-1/2 rounded py-1 cursor-pointer select-none"
+                            :class="
+                                appStore.tab === 'ORDER'
+                                    ? 'text-black bg-white shadow'
+                                    : ''
+                            "
+                            @click="appStore.tab = 'ORDER'"
+                        >
+                            Đơn hàng
+                        </p>
+                    </div>
+                    <AssignedEmployees
+                        v-if="appStore.tab === 'USER'"
+                        :list_employee="list_employee"
+                    />
+                    <div v-if="appStore.tab === 'ORDER'">
+                        <p class="text-center py-2">
+                            Tính năng đang được phát triển...
+                        </p>
+                    </div>
+                </div>
+                <div v-if="WIDGET.is_admin" class="flex justify-end">
+                    <p
+                        class="bg-slate-200 text-slate-700 py-1 px-2 rounded-md cursor-pointer text-xs font-medium select-none"
+                        @click="appStore.tab = 'SETTING'"
+                    >
+                        Thiết lập
+                    </p>
+                </div>
+            </div>
+            <!-- FORM -->
+            <Setting
+                v-if="['SETTING', 'SETTING_NO_TOKEN'].includes(appStore?.tab)"
+            />
         </div>
-      </div>
-      <!-- FORM -->
-      <Setting v-if="['SETTING', 'SETTING_NO_TOKEN'].includes(appStore?.tab)" />
     </div>
-  </div>
 </template>
 <script setup lang="ts">
 //* import function
@@ -123,11 +130,11 @@ const list_employee = ref<any>([])
 
 // lắng nghe sự kiện từ chatbox
 WIDGET.onEvent(async () => {
-  load()
+    load()
 })
 
 onMounted(async () => {
-  load()
+    load()
 })
 
 // xuất hàm đồng bộ data cho các component con dùng
@@ -135,96 +142,96 @@ provide(keySynchData, synchData)
 
 /** hàm call API đồng bộ dữ liệu sang merchant */
 async function synchData(token_business: string) {
-  try {
-    // ghi lại thông tin khách hàng mới
-    appStore.data_client = await WIDGET.decodeClient()
+    try {
+        // ghi lại thông tin khách hàng mới
+        appStore.data_client = await WIDGET.decodeClient()
 
-    // nếu không có thông tin khách hàng thì không đồng bộ dữ liệu
-    if (!size(appStore.data_client)) return
-    // call API đóng bộ dữ liệu sang merchant
-    let result = await request({
-      uri: 'https://api-contact.merchant.vn/contact/chatbox_sync_contact',
-      method: 'POST',
-      body: appStore.data_client,
-      json: true,
-      headers: {
-        'token-business': token_business,
-      },
-    })
-    // nếu đồng bộ thành công thì lấy danh sách nhân viên phụ trách khách hàng
-    // và thông tin khác hàng đó
-    if (!result.id) throw { message: 'Đồng bộ merchant thất bại' }
-    list_employee.value = result.assigned_employees || []
-    appStore.customer_info = {
-      avatar: result?.avatar,
-      phone: result?.suggest_phone,
-      id_contact_merchant: result?.identifier_id,
+        // nếu không có thông tin khách hàng thì không đồng bộ dữ liệu
+        if (!size(appStore.data_client)) return
+        // call API đóng bộ dữ liệu sang merchant
+        let result = await request({
+            uri: 'https://api-contact.merchant.vn/contact/chatbox_sync_contact',
+            method: 'POST',
+            body: appStore.data_client,
+            json: true,
+            headers: {
+                'token-business': token_business,
+            },
+        })
+        // nếu đồng bộ thành công thì lấy danh sách nhân viên phụ trách khách hàng
+        // và thông tin khác hàng đó
+        if (!result.id) throw { message: 'Đồng bộ merchant thất bại' }
+        list_employee.value = result.assigned_employees || []
+        appStore.customer_info = {
+            avatar: result?.avatar,
+            phone: result?.suggest_phone,
+            id_contact_merchant: result?.identifier_id,
+        }
+        //lấy danh sách tất cả nhân viên
+        await fetchAllEmployee(
+            commonStore.id_business,
+            commonStore.token_business,
+            list_employee.value
+        )
+    } catch (error) {
+        console.log('synch to merchant', error)
+        throw error
     }
-    //lấy danh sách tất cả nhân viên
-    await fetchAllEmployee(
-      commonStore.id_business,
-      commonStore.token_business,
-      list_employee.value
-    )
-  } catch (error) {
-    console.log('synch to merchant', error)
-    throw error
-  }
 }
 /** hàm xử lý lấy tất cả nhân viên */
 async function fetchAllEmployee(
-  /** id doanh nghiệp */
-  id_business: string,
-  /** token doanh nghiệp */
-  token_business: string,
-  /** danh sách nhân viên được giao phụ trách khách hàng */
-  list_employee: string[]
+    /** id doanh nghiệp */
+    id_business: string,
+    /** token doanh nghiệp */
+    token_business: string,
+    /** danh sách nhân viên được giao phụ trách khách hàng */
+    list_employee: string[]
 ) {
-  try {
-    const GETALLEMPLOYEE = EmployeeFetcher(id_business, token_business)
-    GETALLEMPLOYEE.setListAssignedEmployee(list_employee)
-    /** lấy danh sách toàn bộ nhân viên */
-    let result = await GETALLEMPLOYEE.getAllEmployee()
-    console.log('getAllEmployee', result)
+    try {
+        const GETALLEMPLOYEE = EmployeeFetcher(id_business, token_business)
+        GETALLEMPLOYEE.setListAssignedEmployee(list_employee)
+        /** lấy danh sách toàn bộ nhân viên */
+        let result = await GETALLEMPLOYEE.getAllEmployee()
+        console.log('getAllEmployee', result)
 
-    if (!result) return
-    commonStore.listAllEmployee = result
-  } catch (error) {
-    console.log('get all employee home', error)
-  }
+        if (!result) return
+        commonStore.listAllEmployee = result
+    } catch (error) {
+        console.log('get all employee home', error)
+    }
 }
 /** hàm thực thi khi mở đoạn chat hoặc chuyển đoạn chat */
 async function load() {
-  try {
-    // bật loading
-    commonStore.is_loading_full_screen = true
-    let res: IConfigWidget | null = await WIDGET.getConfig({
-      brand_name: 'widget-merchant',
-      type_config: 'CRM',
-    })
+    try {
+        // bật loading
+        commonStore.is_loading_full_screen = true
+        let res: IConfigWidget | null = await WIDGET.getConfig({
+            brand_name: 'widget-merchant',
+            type_config: 'CRM',
+        })
 
-    // nếu có sẽ vào dashboard và đồng bộ dữ liệu, nếu không sẽ vào form
-    if (!res?.token_business || !res?.id_business) {
-      appStore.tab = 'SETTING_NO_TOKEN'
-      //tắt loading
-      commonStore.is_loading_full_screen = false
-      return
+        // nếu có sẽ vào dashboard và đồng bộ dữ liệu, nếu không sẽ vào form
+        if (!res?.token_business || !res?.id_business) {
+            appStore.tab = 'SETTING_NO_TOKEN'
+            //tắt loading
+            commonStore.is_loading_full_screen = false
+            return
+        }
+        //lưu id_business, token_business vào store
+        commonStore.id_business = res.id_business || ''
+        commonStore.token_business = res.token_business
+        //đồng bộ dữ liệu
+        await synchData(res?.token_business)
+
+        appStore.tab = 'USER'
+        //tắt loading
+        commonStore.is_loading_full_screen = false
+    } catch (error) {
+        console.log('load home', error)
+        //chuyển tab setting
+        appStore.tab = 'SETTING_NO_TOKEN'
+        //tắt loading
+        commonStore.is_loading_full_screen = false
     }
-    //lưu id_business, token_business vào store
-    commonStore.id_business = res.id_business || ''
-    commonStore.token_business = res.token_business
-    //đồng bộ dữ liệu
-    await synchData(res?.token_business)
-
-    appStore.tab = 'USER'
-    //tắt loading
-    commonStore.is_loading_full_screen = false
-  } catch (error) {
-    console.log('load home', error)
-    //chuyển tab setting
-    appStore.tab = 'SETTING_NO_TOKEN'
-    //tắt loading
-    commonStore.is_loading_full_screen = false
-  }
 }
 </script>
