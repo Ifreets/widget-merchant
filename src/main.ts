c952509928f07fa1988119bb7b19ed9acf5efb29
bbhhainx <hainx.bbh@gmail.com>
2024-07-01 18:01:10 +0700

    ui create order

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,21 @@
 import { waterfall } from 'async'
 import { createApp } from 'vue'
 import App from '@/App.vue'
+import FloatingVue from 'floating-vue'
 import { loadEnv, loadApp, loadRouter } from '@/service/core'
-import '@/assets/css/tailwind.css'
-import WIDGET from 'bbh-chatbox-widget-js-sdk'
 import type { CbError } from '@/service/interface'
+import WIDGET from 'bbh-chatbox-widget-js-sdk'
 import './assets/css/index.scss'
+import '@/assets/css/tailwind.css'
+import 'floating-vue/dist/style.css'
 
 // [optional] kích hoạt chế độ debug
 WIDGET.debugOn()
 
 const APP = createApp(App)
+
+// * Sử dụng vue tootltip
+APP.use(FloatingVue)
 
 waterfall(
   [
