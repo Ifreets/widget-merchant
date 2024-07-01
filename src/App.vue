35880bdd7c7ebefe8696e57dac4ce01b4ed1c540
bbhhainx <hainx.bbh@gmail.com>
2024-07-01 14:06:10 +0700

    ui order continue

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,16 @@
     <div class="h-full overflow-hidden overflow-y-auto">
       <router-view />
     </div>
+    <Color />
   </div>
 </template>
 
 <script setup lang="ts">
 import { useCommonStore } from '@/stores'
+
+//* components
 import Loading from '@/components/Loading.vue'
+import Color from '@/components/Color.vue'
 
 const commonStore = useCommonStore()
 </script>
---
+++ src/components/Color.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="bg-white text-white border-white bg-black text-black border-black bg-red-500 text-red-500 border-red-500 bg-green-500 text-green-500 border-green-500 bg-blue-500 text-blue-500 border-blue-500 bg-sky-500 text-sky-500 border-sky-500 bg-stone-500 text-stone-500 border-stone-500 bg-pink-500 text-pink-500 border-pink-500 bg-orange-500 text-orange-500 border-orange-500 bg-purple-500 text-purple-500 border-purple-500 bg-blue-700 text-blue-700 border-blue-700"
+  ></div>
+</template>
+
+<script setup lang="ts"></script>
