a584036dd761c5fdeaedc4e7502263e069367f87
bbhhainx <hainx.bbh@gmail.com>
2024-12-04 11:50:57 +0700

    fix ui

--- src/views/order/DetailReportContact.vue
+++ src/views/order/DetailReportContact.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="flex items-center w-full pb-2"
+    v-if="$merchant.current_tab === 'ORDERS'"
     :class="{
       'justify-between':customer_data?.report_order,
       'justify-end':!customer_data?.report_order
@@ -36,7 +37,7 @@
         </p>
       </div>
       <template #popper>
-        <div class="py-2.5 px-3 font-medium flex flex-col gap-1 w-44">
+        <div class="py-2.5 px-3 font-medium flex flex-col gap-1 w-44 text-sm">
           <p class="text-xs">Thông tin đơn của khách trên hệ thống:</p>
           <div class="flex justify-between">
             <p class="text-green-500">Đơn thành công</p>
@@ -56,7 +57,7 @@
     <div class="flex gap-2">
       <a
         v-if="$app.data_client?.conversation_contact?.client_phone"
-        :href="`zalo://conversation?phone=${$app.data_client?.conversation_contact?.client_phone}}`"
+        :href="`zalo://conversation?phone=${$app.data_client?.conversation_contact?.client_phone}`"
         target="_blank"
         v-tooltip="'Xem Zalo'"
       >
@@ -79,11 +80,11 @@
         <FacebookIcon class="w-5 h-5" />
       </a>
       <a
-        v-if="order_edit?.contact_id"
+        v-if="contact?.identifier_id"
         class="flex-grow-0 flex-shrink-0 max-h-fit rounded border border-blue-700"
-        :href="`https://merchant.vn/a/contact?id=${order_edit?.contact_id}`"
+        :href="`https://merchant.vn/a/contact?id=${contact?.identifier_id}`"
         target="_blank"
-        v-tooltip="'Chi tiết'"
+        v-tooltip="'Thông tin khách hàng'"
       >
         <UserCircleIcon class="w-5 h-5 fill-blue-700" />
       </a>
@@ -113,7 +114,7 @@ import type { PhoneData } from '@/service/interface'
 // * stores
 const $app = useAppStore()
 const $merchant = useMerchantStore()
-const { order_edit } = storeToRefs($merchant)
+const { order_edit,contact } = storeToRefs($merchant)
 
 const $toast = new Toast()
 
@@ -148,8 +149,8 @@ async function getReporContact() {
         phone: $app.data_client?.conversation_contact?.client_phone,
       },
     })
-  } catch (error) {
-    $toast.error(error)
+  } catch (e) {
+    console.log(e);
   }
 }
 
